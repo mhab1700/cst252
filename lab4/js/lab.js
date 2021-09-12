355f036e8bb9bd9c157f89e37a9d3a23b1c8a191
mhab1700 <mhabrial@csumb.edu>
2021-09-11 20:16:13 -0400

    Psuedocode

---
+++ lab4/js/lab.js
@@ -0,0 +1,14 @@
+// @summary  Psuedo code on how to animate out an object that is being scrolled out of the viewport.
+// @description  a list of psuedocode to describe what will go into code to fade out a scrolled past element.
+// @link   URL
+// @file   cst252.
+// @author MatthewHabrial.
+// @since  09.11.2021
+
+
+// define element class .js-scroll, and .js-scrolled
+// make .js-scroll hide whenever .js-scrolled is applied to it
+// target class .js-scroll
+// detect when element is in view by finding the distance of the element from the top of the page
+// add the scrolled class to the .js-scroll element with for each
+// pass into a scroll event listener
