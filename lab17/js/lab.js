889bf2f6ec1958a17f8f193c5dea08b1915ca571
mhab1700 <mhabrial@csumb.edu>
2021-11-02 18:43:55 -0400

    csdc

---
+++ lab17/js/lab.js
@@ -0,0 +1,30 @@
+//@authors Joshua Rodriguez, Matthew HAbrial
+//@date 11/2/21
+
+
+class Vehicle {
+  constructor(make, model, year, color, extras) {
+          this.carMake = make;
+          this.carModel = model;
+          this.carYear = year;
+          this.carColor = color;
+          this.carExtras = extras;
+}
+          info() {
+            return this.carMake + " " + this.carModel + " " +
+                   this.carYear + " " + this.carColor + " " +
+                   this.carExtras + " ";
+    }
+}
+
+
+// all cars
+var car = new Vehicle("Nissan", "Altima", 2007, "Silver", "Dented");
+var car1 = new Vehicle("Nissan", "Sentra", 2010, "Blue", "Minty");
+var car2 = new Vehicle("Ford", "Pinto", 1980, "Brass", "Scratched");
+
+
+//output
+$("#output").append("<p>" + car.info() + "</p>");
+$("#output").append("<p>" + car1.info() + "</p>");
+$("#output").append("<p>" + car2.info() + "</p>");
